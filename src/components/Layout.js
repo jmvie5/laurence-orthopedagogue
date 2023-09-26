40eafb4ef8ba848e04e83d9ed62c3ac6f05e74a9
jmvie5 <jeanmichel.viel@gmail.com>
2023-09-26 11:34:46 -0400

    blog post boite-aux-lettres

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,9 @@
-import * as React from 'react'
-import { Link } from 'gatsby'
-import { StaticImage } from "gatsby-plugin-image"
-import useWindowSize from '../../modules/windowSize.js'
-import DropMenu from './DropMenu.js'
-import { useSiteMetadata } from "../hooks/use-site-metadata"
-
+import * as React from "react";
+import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
+import useWindowSize from "../../modules/windowSize.js";
+import DropMenu from "./DropMenu.js";
+import { useSiteMetadata } from "../hooks/use-site-metadata";
 
 /* Lien facebook
 <a href='https://www.facebook.com/' className='flex space-x-1.5'>
@@ -15,81 +14,127 @@ import { useSiteMetadata } from "../hooks/use-site-metadata"
 */
 
 const Layout = ({ pageTitle, children }) => {
+  const siteMetadata = useSiteMetadata();
 
-    const siteMetadata = useSiteMetadata()
+  let isSmallWindow;
+  let windowWidth = useWindowSize().width;
 
-    let isSmallWindow
-    let windowWidth = useWindowSize().width
+  if (windowWidth < 700) {
+    isSmallWindow = true;
+  } else if (windowWidth > 700) {
+    isSmallWindow = false;
+  }
 
-    if (windowWidth < 700) {
-        isSmallWindow = true;
-    } else if (windowWidth > 700) {
-        isSmallWindow = false;
-    };
-
-    return (
-        <div>
-            <div className='flex bg-lbf-blanc text-white min-h-screen justify-center'>
-                <div className=" flex flex-col justify-between max-w-screen-xl w-full">
-                    <header className='flex justify-between items-center p-4 sm:p-8  z-50'>
-                        <title>{pageTitle} | {siteMetadata.title}</title>
-                        <Link to="/" className="xxs:text-4xl text-2xl font-satisfy bg-lbf-orange p-4 mr-2 rounded-lg">
-                            <p>Laurence bf,</p>
-                            <p>orthopédagogue</p>
+  return (
+    <div>
+      <div className="flex bg-lbf-blanc text-white min-h-screen justify-center">
+        <div className=" flex flex-col justify-between max-w-screen-xl w-full">
+          <header className="flex justify-between items-center p-4 sm:p-8  z-50">
+            <title>
+              {pageTitle} | {siteMetadata.title}
+            </title>
+            <Link
+              to="/"
+              className="xxs:text-4xl text-2xl font-satisfy bg-lbf-orange p-4 mr-2 rounded-lg"
+            >
+              <p>Laurence bf,</p>
+              <p>orthopédagogue</p>
+            </Link>
+            <div>
+              {isSmallWindow ? (
+                <div> {DropMenu()} </div>
+              ) : (
+                <nav className="bg-lbf-orange p-4 rounded-lg">
+                  <ul className="grid grid-rows-2 lg:grid-rows-1 grid-flow-col gap-2 lg:divide-x-2 justify-center">
+                    {siteMetadata.menuLinks.map((link) => (
+                      <li key={link.name} className="place-self-center px-4">
+                        <Link
+                          className="hover:text-black text-xl font-mulish"
+                          to={link.link}
+                        >
+                          {link.name}
                         </Link>
-                        <div>
-                            {isSmallWindow
-                            ? <div> {DropMenu()} </div>
-                            : <nav className='bg-lbf-orange p-4 rounded-lg'>
-                                <ul className="grid grid-rows-2 lg:grid-rows-1 grid-flow-col gap-2 lg:divide-x-2 justify-center">
-                                {siteMetadata.menuLinks.map(link => (
-                                    <li
-                                    key={link.name}
-                                    className="place-self-center px-4">
-                                        <Link className="hover:text-black text-xl font-mulish" to={link.link}>
-                                            {link.name}
-                                        </Link>
-                                    </li>
-                                ))}
-                                </ul>
-                            </nav>
-                            }
-                        </div>
-                    </header>
-                    <div className='bg-lbf-blanc min-h-screen'>
-                        <main className="font-sans text-black m-4 xs:m-8">
-                            <h1 className='text-3xl font-bold pb-8 font-mulish'>{pageTitle}</h1>
-                            {children}
-                        </main>
-                    </div>
-                </div>
-                
+                      </li>
+                    ))}
+                  </ul>
+                </nav>
+              )}
             </div>
-            <div className='p-4 bg-gradient-to-t from-lbf-orange to-lbf-blanc'/>
-            <footer className='flex flex-col sm:flex-row text-white bg-lbf-orange p-4 gap-4 text-sm md:text-base justify-between' >
-                <div className=''>
-                    <div className='mt-2 flex justify-center'>
-                        <div className='flex flex-col xl:flex-row xl:space-x-4 space-y-2 xl:space-y-0'>
-                            <a href='https://www.instagram.com/laurencebf.orthopedagogue/' className='flex space-x-1.5 group'>
-                                <StaticImage src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/800px-Instagram_logo_2022.svg.png'
-                                    alt="Logo d'Instagram" width={30} height={30} className='aspect-square'/>
-                                <p className='group-hover:underline font-mulish'>@laurencebf_orthopedagogue</p>
-                            </a>
-                            <a href='https://www.pinterest.ca/laurencebforthopedagogue/' className='flex space-x-1.5 group'>
-                                <StaticImage src="https://upload.wikimedia.org/wikipedia/commons/0/08/Pinterest-logo.png" alt="Pinterest-logo" width={30} height={30} className='aspect-square'/>
-                                <p className='font-mulish group-hover:underline'>@laurencebforthopedagogue</p>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div className='flex flex-col sm:mr-4 gap-2 items-center'>
-                    <p>Contact : <a className="hover:underline" href='mailto:info@laurencebf.ca'>info@laurencebf.ca</a></p>
-                    <p className=''>Site web développé par <a className="hover:underline" href='https://jeanmichelviel.ca'>jeanmichelviel.ca</a></p>
-                </div>
-            </footer>
+          </header>
+          <div className="bg-lbf-blanc min-h-screen">
+            <main className="font-sans text-black m-4 xs:m-8">
+              <h1 className="text-3xl font-bold pb-8 font-mulish">
+                {pageTitle}
+              </h1>
+              {children}
+            </main>
+          </div>
         </div>
-        
-    )
-}
+      </div>
+      <div className="p-4 bg-gradient-to-t from-lbf-orange to-lbf-blanc" />
+      <footer className="flex flex-col sm:flex-row text-white bg-lbf-orange p-4 gap-4 text-sm md:text-base justify-between">
+        <div className="">
+          <div className="mt-2 flex justify-center">
+            <div className="flex flex-col xl:flex-row xl:space-x-4 space-y-2 xl:space-y-0">
+              <a
+                href="https://www.instagram.com/laurencebf.orthopedagogue/"
+                className="flex space-x-1.5 group"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <StaticImage
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/800px-Instagram_logo_2022.svg.png"
+                  alt="Logo d'Instagram"
+                  width={30}
+                  height={30}
+                  className="aspect-square"
+                />
+                <p className="group-hover:underline font-mulish">
+                  @laurencebf_orthopedagogue
+                </p>
+              </a>
+              <a
+                href="https://www.pinterest.ca/laurencebforthopedagogue/"
+                className="flex space-x-1.5 group"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <StaticImage
+                  src="https://upload.wikimedia.org/wikipedia/commons/0/08/Pinterest-logo.png"
+                  alt="Pinterest-logo"
+                  width={30}
+                  height={30}
+                  className="aspect-square"
+                />
+                <p className="font-mulish group-hover:underline">
+                  @laurencebforthopedagogue
+                </p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col sm:mr-4 gap-2 items-center">
+          <p>
+            Contact :{" "}
+            <a className="hover:underline" href="mailto:info@laurencebf.ca">
+              info@laurencebf.ca
+            </a>
+          </p>
+          <p className="">
+            Site web développé par{" "}
+            <a
+              className="hover:underline"
+              href="https://jeanmichelviel.ca"
+              target="_blank"
+              rel="noreferrer"
+            >
+              jeanmichelviel.ca
+            </a>
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,85 +1,120 @@
-import * as React from 'react'
-import Layout from '../components/Layout'
-import { StaticImage, GatsbyImage, getImage } from 'gatsby-plugin-image'
-import { Seo } from '../components/seo'
-import { Link, graphql } from 'gatsby'
+import * as React from "react";
+import Layout from "../components/Layout";
+import { StaticImage, GatsbyImage, getImage } from "gatsby-plugin-image";
+import { Seo } from "../components/seo";
+import { Link, graphql } from "gatsby";
 
 //Newsletter : https://javascript.plainenglish.io/how-to-create-a-newsletter-with-mailchimp-gatsby-js-netlify-d48778d5c774
 
-const IndexPage = ({data}) => {
-
-return (
+const IndexPage = ({ data }) => {
+  return (
     <Layout pageTitle="">
-        <div className='flex flex-col gap-8'>
-            <div className='flex flex-col sm:inline bg-lbf-orange lg:ml-40 p-4 rounded-lg gap-2 shadow-md shadow-gray-500 font-mulish'>
-                <p className='text-xl font-bold sm:pb-2'>Laurencebf, orthopédagogue</p>
-                <StaticImage src='../images/laurence_bibli.webp' className='float-right self-center bg-contain bg-top h-96 min-[340px]:w-72 m-2 rounded-xl' alt='Photo de Laurence Bernard-Fontaine'/>
-                <p className='text-lg'>Laurence Bernard-Fontaine est l’orthopédagogue derrière la page Laurence bf, orthopédagogue. Elle est spécialisée en difficultés d’apprentissage en français (lecture/écriture) auprès d’enfants de tous âges. Comme elle a le désir d’aider le plus d’enfants possible à développer leurs compétences littéraciques (lecture-écriture), elle a décidé de créer l’espace Laurence bf, orthopédagogue pour rejoindre les parents, les acteurs les plus importants dans la vie des enfants. Son but est de soutenir et d’aider les parents qui se questionnent sur les façons d’intervenir auprès de leur enfant qui rencontre des difficultés en français. 
-                </p>
-            </div>
-            <div className='flex flex-col items-center pt-4'>
-                <div className="text-xl pb-4 font-mulish">
-                    Articles récemment publiés
-                </div>
-                <div>
-                    <div className="grid grid-col-1 md:grid-cols-3 gap-8">
-                    {
-                        data.allMdx.nodes.map(node => (
-                            <Link to={`/blogue/${node.frontmatter.slug}`} >
-                                <article key={node.id} className="flex flex-col gap-2 bg-lbf-orange shadow-md shadow-gray-500 rounded-lg p-4 justify-between place-items-center transition ease-in-out duration-300 hover:scale-105 h-full">
-                                    <h2 className="font-bold text-xl text-center font-mulish">
-                                        {node.frontmatter.title}
-                                    </h2>
-                                    <GatsbyImage image={getImage(node.frontmatter.image)} alt={node.frontmatter.image_alt} className="rounded-xl max-w-xs bg-contain"/>
-                                    <p className='font-mulish'>Publication : {node.frontmatter.date}</p>
-                                </article>
-                            </Link>
-                                
-                        ))
-                    }
-                    </div>
-                </div>
-            </div>
-            <div className='flex flex-col items-center'>
-                <div className="text-xl pb-4 font-mulish self-center">
-                    Suivez-moi sur Instagram!
-                </div>
-                <div className='grid grid-col-1 sm:grid-cols-2 xl:grid-cols-4 gap-8 place-items-center'>
-                    <iframe src='https://www.instagram.com/p/CuCc2iSrUMs/embed' className='h-96 rounded-lg shadow' title="Réviser en s'amusant avec les cartes éducatives"></iframe>
-                    <iframe src='https://www.instagram.com/p/CtzAHDPL1Lw/embed' className='h-96 rounded-lg shadow' title='La fée des livres'></iframe>
-                    <iframe src='https://www.instagram.com/p/Ctg-mTer-H_/embed' className='h-96 rounded-lg shadow' title="Pause lecture!"></iframe>
-                    <iframe src='https://www.instagram.com/p/CtO8-MdrXIB/embed' className='h-96 rounded-lg shadow' title="Stratégies d'études, la récupération"></iframe>
-                </div>
-            </div>
+      <div className="flex flex-col gap-8">
+        <div className="flex flex-col sm:inline bg-lbf-orange lg:ml-40 p-4 rounded-lg gap-2 shadow-md shadow-gray-500 font-mulish">
+          <p className="text-xl font-bold sm:pb-2">
+            Laurencebf, orthopédagogue
+          </p>
+          <StaticImage
+            src="../images/laurence_bibli.webp"
+            className="float-right self-center bg-contain bg-top h-96 min-[340px]:w-72 m-2 rounded-xl"
+            alt="Photo de Laurence Bernard-Fontaine"
+          />
+          <p className="text-lg">
+            Laurence Bernard-Fontaine est l’orthopédagogue derrière la page
+            Laurence bf, orthopédagogue. Elle est spécialisée en difficultés
+            d’apprentissage en français (lecture/écriture) auprès d’enfants de
+            tous âges. Comme elle a le désir d’aider le plus d’enfants possible
+            à développer leurs compétences littéraciques (lecture-écriture),
+            elle a décidé de créer l’espace Laurence bf, orthopédagogue pour
+            rejoindre les parents, les acteurs les plus importants dans la vie
+            des enfants. Son but est de soutenir et d’aider les parents qui se
+            questionnent sur les façons d’intervenir auprès de leur enfant qui
+            rencontre des difficultés en français.
+          </p>
         </div>
-        
+        <div className="flex flex-col items-center pt-4">
+          <div className="text-xl pb-4 font-mulish">
+            Articles récemment publiés
+          </div>
+          <div>
+            <div className="grid grid-col-1 md:grid-cols-3 gap-8">
+              {data.allMdx.nodes.map((node) => (
+                <Link to={`/blogue/${node.frontmatter.slug}`}>
+                  <article
+                    key={node.id}
+                    className="flex flex-col gap-2 bg-lbf-orange shadow-md shadow-gray-500 rounded-lg p-4 justify-between place-items-center transition ease-in-out duration-300 hover:scale-105 h-full"
+                  >
+                    <h2 className="font-bold text-xl text-center font-mulish">
+                      {node.frontmatter.title}
+                    </h2>
+                    <GatsbyImage
+                      image={getImage(node.frontmatter.image)}
+                      alt={node.frontmatter.image_alt}
+                      className="rounded-xl max-w-xs bg-contain"
+                    />
+                    <p className="font-mulish">
+                      Publication : {node.frontmatter.date}
+                    </p>
+                  </article>
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col items-center">
+          <div className="text-xl pb-4 font-mulish self-center">
+            Suivez-moi sur Instagram!
+          </div>
+          <div className="grid grid-col-1 sm:grid-cols-2 xl:grid-cols-4 gap-8 place-items-center">
+            <iframe
+              src="https://www.instagram.com/p/CxDSElZr_4m/embed"
+              className="h-96 rounded-lg shadow"
+              title="Réviser en s'amusant avec les cartes éducatives"
+            ></iframe>
+            <iframe
+              src="https://www.instagram.com/p/Cv9wSS1g4xD/embed"
+              className="h-96 rounded-lg shadow"
+              title="La fée des livres"
+            ></iframe>
+            <iframe
+              src="https://www.instagram.com/p/Cvrs2rnvOaH/embed"
+              className="h-96 rounded-lg shadow"
+              title="Pause lecture!"
+            ></iframe>
+            <iframe
+              src="https://www.instagram.com/p/CuCc2iSrUMs/embed"
+              className="h-96 rounded-lg shadow"
+              title="Stratégies d'études, la récupération"
+            ></iframe>
+          </div>
+        </div>
+      </div>
     </Layout>
-)
-}
+  );
+};
 
 export const query = graphql`
-    query {
-        allMdx(sort: { frontmatter: { date: DESC }}, limit: 3) {
-            nodes {
-                frontmatter {
-                    date(formatString: "DD-MM-YYYY")
-                    title
-                    slug
-                    summary
-                    image {
-                        childImageSharp {
-                          gatsbyImageData
-                        }
-                    }
-                    image_alt
-                }
-                id
+  query {
+    allMdx(sort: { frontmatter: { date: DESC } }, limit: 3) {
+      nodes {
+        frontmatter {
+          date(formatString: "DD-MM-YYYY")
+          title
+          slug
+          summary
+          image {
+            childImageSharp {
+              gatsbyImageData
             }
+          }
+          image_alt
         }
-    }`
+        id
+      }
+    }
+  }
+`;
 
-export default IndexPage
+export default IndexPage;
 
-export const Head = () => (
-<Seo />
-)
+export const Head = () => <Seo />;
